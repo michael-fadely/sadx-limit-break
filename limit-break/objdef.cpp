e7e1961a9e8a568e61cd5d55af08d51554075c2f
Kell <kellspam@outlook.fr>
2019-05-31 18:26:49 +0200

    Hovering helicopters fix

--- limit-break/objdef.cpp
+++ limit-break/objdef.cpp
@@ -8,13 +8,20 @@ void Ring_Main_r(ObjectMaster *a1) {
 	Ring_Main(a1);
 }
 
+float __stdcall squareroot_r(float a1) {
+	return 0;
+}
+
 void objdef_init() {
 	float range = 3600000;
 
 	//Bridge in EC01
 	WriteData((float*)0x7EC9F8, range);
 	WriteData((float*)0x7EC9F4, range);
-
+	
+	//Hovering helicopters
+	WriteCall((void**)0x6137E8, squareroot_r);
+	
 	//SkyDeck ring hack
 	WriteData((ObjectFuncPtr*)0x2230F2C, Ring_Main_r);
 }
